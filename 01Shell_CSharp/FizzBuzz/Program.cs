1452be444b6ab8f3722eab483f86596602f4f3af
Juniper Song <songminseonk@gmail.com>
2022-06-21 16:46:53 -0400

    fizzbuzz instruction

--- 01Shell_CSharp/FizzBuzz/Program.cs
+++ 01Shell_CSharp/FizzBuzz/Program.cs
@@ -37,3 +37,88 @@ Console.WriteLine(true && true); // true;
 Console.WriteLine(false && true); //false
 // || OR
 Console.WriteLine(true || false); //true
+
+//Basic Data Types
+//string, bool, int, double (for decimal #), long (a long int), byte, short (a short int), char (a single character), decimal
+//The ones you definitely want to be familiar with is string, bool, int, double
+
+//Conditional Statements: If/Else
+if(true)
+{
+    //act certain way
+}
+else
+{
+    //do something else
+}
+
+if(name == "juniper")
+{
+    Console.WriteLine("Hi Juniper!");
+}
+else
+{
+    Console.WriteLine("you're not juniper");
+}
+
+if(x < 0)
+{
+    Console.WriteLine("x is less than 0");
+}
+else if(0 < x && x < 10)
+{
+    Console.WriteLine("x is between 0 and 10");
+}
+else
+{
+    Console.WriteLine("x is greater than 10");
+}
+
+if(x % 2 == 0)
+{
+    Console.WriteLine("x is divisible by 2!");
+}
+else if(x % 3 == 0)
+{
+    Console.WriteLine("x is divisible by 3");
+}
+else
+{
+    Console.WriteLine("x is neither divisible by 2 or 3");
+}
+
+//For Loop
+int n = 1;
+//do stuff here
+n = n + 1;
+//do more stuff here
+n = n + 1;
+//do even more stuff here
+// Instead of ^
+// if you want to perform same action over and over for a certain number of times or until certain condition is met, we can use a loop!
+// for loop
+// create a new integer variable i and set it to 0,
+// while i is less than 100, so i from 0 to 99, execute the code block below
+// and every loop it executes, add 1 to i.
+
+for(int i = 0; i < 100; i++)
+{
+    Console.WriteLine(i);
+}
+
+/*
+This is multiline comment, or highlight a text block, and press ctrl + /
+*/
+
+//For rest of today and until 11AM tomorrow, let's group up and write FizzBuzz program
+//This is simple program that will loop from 1 to a user defined number
+//that will print out the number on the screen
+//Except when the number is divisible by 3, it prints 'fizz'
+//and if the number is divisible by 5 it prints 'buzz'
+//and if the number is divisible by 3 and 5, it prints 'fizzbuzz'
+
+//First, ask the user to provide the end number
+//and then print from 1 to the end number, replacing multiples of 3 with fizz
+//and multiples of 5 with buzz
+// Fizzbuzz from 1 to 16
+//1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16

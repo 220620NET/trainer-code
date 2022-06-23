8c37af219d3250503bfe225b75f326b859bee92a
Juniper Song <songminseonk@gmail.com>
2022-06-23 13:00:28 -0400

    started on pokestorageapp

---
+++ 01Shell_CSharp/PokemonStorageSystem/Program.cs
@@ -0,0 +1,9 @@
+﻿using Models;
+
+PokeTrainer trainer = new PokeTrainer();
+
+//Doesn't work, access denied
+// trainer.name = "Ash Ketchum";
+
+trainer.SetName("Ash Ketchum");
+Console.WriteLine(trainer.GetName());
